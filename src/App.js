c915840816de930364235f70f22cdcfa9d3cc2df
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-13 15:37:31 +0000

    Testing changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003366',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  heading: {
+    fontSize: 24,
+    paddingBottom: 10,
+    color: '#fff',
   },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
+      <Text style={styles.heading}>Gym Tracker v0.0.1</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Changes you make will automatically reload.</Text>
+      <Text style={styles.text}>Shake your phone to open the developer menu.</Text>
     </View>
   );
 }
