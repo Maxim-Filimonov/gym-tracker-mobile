79d4f4d0f3a56922b495ee2303e142d5332081a7
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-19 19:35:26 +0000

    Cleanup unused code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './components/LandingPage';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#003366',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  heading: {
-    fontSize: 24,
-    paddingBottom: 10,
-    color: '#fff',
-  },
-});
 
 export default function App() {
   return (
