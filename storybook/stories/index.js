32643d90a6c0e05aec1c911a7267e9fd6acb21eb
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-24 11:31:02 +0000

    Exercise components

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -19,6 +19,57 @@ import TrainingProgramList from '../../src/containers/TrainingProgramList';
 import TrainingProgramListItem from '../../src/components/training-program-select-page/TrainingProgramListItem';
 import TrainingProgramPage from '../../src/components/training-program-select-page/TrainingProgramPage';
 import ProgramHeading from '../../src/components/exercises-page/ProgramHeading';
+import Summary from '../../src/components/exercises-page/Summary';
+import Exercise from '../../src/components/exercises-page/Exercise';
+
+storiesOf('Exercise', module)
+  .add('Exercise - default', () => {
+    const props = {
+      name: 'Dips',
+      lastSession: {
+        week: 'Week 1',
+        day: 'Day 3',
+        weight: '10',
+        reps: 15,
+      },
+      targets: {
+        sets: 1,
+        reps: 5,
+      },
+      ptNote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. ',
+    };
+
+    return <Exercise {...props} />;
+  })
+  .add('Exercise - no last session details', () => {
+    const props = {
+      name: 'Dips',
+      targets: {
+        sets: 1,
+        reps: 5,
+      },
+      ptNote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. ',
+    };
+
+    return <Exercise {...props} />;
+  })
+  .add('Exercise - No PT notes', () => {
+    const props = {
+      name: 'Dips',
+      lastSession: {
+        week: 'Week 1',
+        day: 'Day 3',
+        weight: '10',
+        reps: 15,
+      },
+      targets: {
+        sets: 1,
+        reps: 5,
+      },
+    };
+
+    return <Exercise {...props} />;
+  });
 
 storiesOf('Exercise Program Page', module)
   .add('Program Heading', () => {
@@ -28,6 +79,12 @@ storiesOf('Exercise Program Page', module)
       day: 'day 2',
     };
     return <ProgramHeading {...props} />;
+  })
+  .add('Summary', () => {
+    const props = {
+      text: 'PT Summary ... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. Pellentesque sagittis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. Pellentesque sagitti       text: \'PT Summary ... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. Pellentesque sagittis pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mi lobortis, sagittis augue ac, venenatis enim. Pellentesque sagitti',
+    };
+    return <Summary {...props} />;
   });
 
 storiesOf('Training Program Page', module)
