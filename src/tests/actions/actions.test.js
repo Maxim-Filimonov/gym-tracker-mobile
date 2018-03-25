4f11f6611ebf591141278b8ce391c7a5179931cc
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-25 18:34:45 +0100

    Test login with google actions

--- src/tests/actions/actions.test.js
+++ src/tests/actions/actions.test.js
@@ -14,34 +14,65 @@ describe('async actions', () => {
     fetchMock.restore();
   });
 
-  it('creates FETCH_JWT_SUCCESS when fetching JWT is done', () => {
-    fetchMock
-      .post(
-        `${API_URL}/login`,
-        {
-          body: { email: 'test@test.com', password: null },
-          headers: { 'content-type': 'application/json' },
-        },
-      );
+  describe('fetchJWT', () => {
+    it('creates FETCH_JWT_SUCCESS when fetching JWT is done', () => {
+      fetchMock
+        .post(
+          `${API_URL}/login`,
+          {
+            body: { email: 'test@test.com', password: null },
+            headers: { 'content-type': 'application/json' },
+          },
+        );
 
-    const expectedActions = [
-      { type: types.FETCH_JWT_REQUEST, loading: true },
-      { type: types.FETCH_JWT_SUCCESS, jwt: undefined },
-    ];
+      const expectedActions = [
+        { type: types.FETCH_JWT_REQUEST, loading: true },
+        { type: types.FETCH_JWT_SUCCESS, jwt: undefined },
+      ];
 
-    const store = mockStore({ });
+      const user = {
+        socialUserId: 'test',
+        image: 'image.jpg',
+        email: 'test@test.com',
+        name: 'test',
+        socialAppTokens: { type: 'Google' },
+      };
 
-    const user = {
-      socialUserId: 'test',
-      image: 'image.jpg',
-      email: 'test@test.com',
-      name: 'test',
-      socialAppTokens: { type: 'Google' },
-    };
+      const store = mockStore({ });
 
-    return store.dispatch(actions.fetchJWT(user)).then(() => {
-      // return of async actions
-      expect(store.getActions()).toEqual(expectedActions);
+      return store.dispatch(actions.fetchJWT(user)).then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
+  });
+
+  describe('loginWithGoogle', () => {
+    it('creates a LOGIN_GOOGLE_SUCCESS when logging in a user with Google', () => {
+      const mockUser = {
+        socialUserId: 'test',
+        image: 'image.jpg',
+        email: 'test@test.com',
+        name: 'test',
+        socialAppTokens: { type: 'Google' },
+      };
+
+      // actions.processGoogleLogin = jest.fn().mockImplementation(() =>
+      //   Promise.resolve({
+      //     user: { ...mockUser },
+      //   }));
+
+      const expectedActions = [
+        { type: types.LOGIN_GOOGLE_REQUEST, loading: true },
+        { type: types.LOGIN_GOOGLE_SUCCESS, user: { ...mockUser } },
+      ];
+
+      const store = mockStore({ });
+
+      return store.dispatch(actions.loginWithGoogle()).then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions);
+      });
     });
   });
 });
