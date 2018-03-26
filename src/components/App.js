51e64588bc653e72c195456082c4bed9c6127a74
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-26 17:10:34 +0100

    refactor to get isLoggedIn from state

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import LandingPage from './landing-page/LandingPage';
+import TrainingProgramPage from './training-program-select-page/TrainingProgramPage';
 
-const App = () => <LandingPage />;
+const App = ({ isLoggedIn }) => (isLoggedIn ? <TrainingProgramPage /> : <LandingPage />);
 
-export default App;
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  isLoggedIn: state.user.gymTrackerJWT ? true : false,
+});
+
+export default connect(mapStateToProps)(App);
