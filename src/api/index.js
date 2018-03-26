c00cfc237fe3000f290e0db2a5ee51bf8252ecf9
Alex Bandisch <alexander.bandisch@gmail.com>
2018-03-26 20:19:15 +0100

    Obfuscate mock login data

--- src/api/index.js
+++ src/api/index.js
@@ -22,10 +22,15 @@ export const fakeFacebookLogin = (returnSuccess = true) =>
     .then(() => (
       returnSuccess ?
         Promise.resolve({
-          socialUserId: '1747241378665802',
-          name: 'Alex Bandisch',
-          email: 'alex@bandisch.com',
+          socialUserId: '0000000000000001',
+          name: 'Dummy Test-Guy',
+          email: 'test@user.com',
           image: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12998676_1087630357960244_4882584167328260793_n.jpg?oh=3b85a0ae065c63668282cca5f445aea9&oe=5B2F64C7',
+          socialAppTokens: {
+            type: 'Facebook',
+            accessToken: 'myfakeaccesscodeortokenorwoteva',
+            expires: 1527275629,
+          },
         }) :
         Promise.reject(new Error('Failed logging in with Google'))
     ));
@@ -35,17 +40,17 @@ export const fakeGoogleLogin = (returnSuccess = true) =>
     .then(() => (
       returnSuccess ?
         Promise.resolve({
-          email: 'alexander.bandisch@gmail.com',
+          email: 'test.userh@gmail.com',
           image: 'https://lh3.googleusercontent.com/-c2s_eAAdRP8/AAAAAAAAAAI/AAAAAAAACGE/jreSaPOAvb0/photo.jpg',
-          name: 'Alex Bandisch',
+          name: 'Test User',
           socialAppTokens: {
             type: 'Google',
-            idToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjM3NmVhMWUyZjRjOTM3Yz…db8VMEHxNI2UK7CpcsSu8U3ZBU1spYvd5bgiRTCYf9r0vk5eA',
-            refreshToken: '1/1k6gRrB8lUAb2KucMG9wWxX6AN9WCZ_Q2t0HXJxAogE',
-            serverAuthCode: '4/AADIOYW08Yw5BQYnkL2IIRrm7e2Rb2-SZiZz6dydZy7nwmoV3tNrvDRh992hkqhC5VymZHc5tKqI7drawcT-T1w',
-            accessToken: 'ya29.GluKBZkbZ6DMNNm1dKjL04tehpHMLBBLvqFqEnxpqVzaX…EFNnYFaKB6K-qI_6Iyjd01mfriDyoE00FifcXQlr3dvJyOFn_',
+            idToken: 'mygooglyfaceidtokes',
+            refreshToken: 'mygooglyfacerefreshtokes',
+            serverAuthCode: 'mygooglyfaceservertokes',
+            accessToken: 'ohmygoodnessitsmyaccesstokes',
           },
-          socialUserId: '116415253186278481933',
+          socialUserId: '111111111111111111110',
         }) :
         Promise.reject(new Error('Failed logging in with Google'))
     ));
