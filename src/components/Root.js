50f68855a2a5755259b777b8d188c9a9a3c4e7a9
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-02 15:05:56 +0100

    Move GraphQLWrapper to root component

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../configureStore';
 import App from './App';
+import GraphQLWrapper from '../containers/GraphQLWrapper';
 
 const store = configureStore();
 
 const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <GraphQLWrapper>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </GraphQLWrapper>
 );
 
 export default Root;
--- src/components/training-program-select-page/TrainingProgramPage.js
+++ src/components/training-program-select-page/TrainingProgramPage.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { ScrollView, View } from 'react-native';
 import Header from '../Header';
 import AppFooter from '../AppFooter';
-import GraphQLWrapper from '../../containers/GraphQLWrapper';
 import TrainingProgramList from '../../containers/TrainingProgramList';
 import { APP_AUTHOR, APP_NAME, APP_SLOGAN } from '../../config';
 
 const TrainingProgramPage = () => (
-  <GraphQLWrapper>
-    <View style={{ flex: 1 }}>
-      <Header title={APP_NAME} slogan={APP_SLOGAN} />
-      <ScrollView>
-        <TrainingProgramList />
-        <AppFooter appName={APP_NAME} appAuthor={APP_AUTHOR} />
-      </ScrollView>
-    </View>
-  </GraphQLWrapper>
+  <View style={{ flex: 1 }}>
+    <Header title={APP_NAME} slogan={APP_SLOGAN} />
+    <ScrollView>
+      <TrainingProgramList />
+      <AppFooter appName={APP_NAME} appAuthor={APP_AUTHOR} />
+    </ScrollView>
+  </View>
 );
 
 export default TrainingProgramPage;
