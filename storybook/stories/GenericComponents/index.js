72877ca3bd8bc6ab5de87566b64bafc024b539a7
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-02 16:45:54 +0100

    Implement template screen component

--- storybook/stories/GenericComponents/index.js
+++ storybook/stories/GenericComponents/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import Header from '../../../src/components/Header';
 import AppFooter from '../../../src/components/AppFooter';
+import Screen from '../../../src/components/Screen';
+
+storiesOf('Screen', module)
+  .add('default', () =>
+    (
+      <Screen>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce hendrerit felis et massa fringilla, hendrerit bibendum
+          libero rutrum. Mauris volutpat mattis aliquam. Nulla ac orci id
+          massa malesuada posuere. Nulla maximus lectus et rhoncus blandit.
+          Sed a orci ac nulla sodales pharetra. Quisque eget congue lacus.
+            Nam ac arcu metus.
+        </Text>
+      </Screen>
+    ))
+  .add('without footer', () =>
+    (
+      <Screen showFooter={false}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce hendrerit felis et massa fringilla, hendrerit bibendum
+          libero rutrum. Mauris volutpat mattis aliquam. Nulla ac orci id
+          massa malesuada posuere. Nulla maximus lectus et rhoncus blandit.
+          Sed a orci ac nulla sodales pharetra. Quisque eget congue lacus.
+            Nam ac arcu metus.
+        </Text>
+      </Screen>
+    ));
 
 storiesOf('App Header', module)
   .add('default', () => <Header title="Gym Tracker" />)
