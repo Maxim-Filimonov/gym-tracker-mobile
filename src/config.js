6eb6dd7816fa14ac9c934005f058f873884dacd5
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-03 17:23:37 +0100

    Use local graphQL server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 exports.FACEBOOK_APP_ID = '182710952339837';
 exports.GOOGLE_ANDROID_CLIENT_ID = '698412195965-6r19vionf1ehiptgqs49gdoedtd2jmho.apps.googleusercontent.com';
 exports.GOOGLE_IOS_CLIENT_ID = '698412195965-11vilao62ag1r9akvlfqo3nc6pdn7omg.apps.googleusercontent.com';
-exports.GRAPHQL_URI = 'https://strengthexchange-xljplatbdo.now.sh/graphql';
+exports.GRAPHQL_URI = 'http://localhost:3000/graphql';
+// exports.GRAPHQL_URI = 'https://strengthexchange-xljplatbdo.now.sh/graphql';
 exports.APP_NAME = 'GymBuddy';
 exports.APP_SLOGAN = 'strength & exercise tracking';
 exports.APP_AUTHOR = 'Alex Bandisch';
