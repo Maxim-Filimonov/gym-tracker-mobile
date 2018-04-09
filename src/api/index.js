f66270eac70b5cce431d8d92aebcf798a1f71a2a
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-09 10:03:26 +0100

    Remove fake testing login

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import { LOGIN_API_URL, APP_NAME, STR_TRCKR_API_URL } from '../config';
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
 export const fetchJWT = (user) => {
   const init = {
     method: 'POST',
@@ -37,41 +35,3 @@ export const selectProgram = (user, programId, name, dateStarted) => {
     )
     .catch(() => Promise.reject(new Error(`Problem connecting to ${APP_NAME}.`)));
 };
-
-export const fakeFacebookLogin = (returnSuccess = true) =>
-  delay(500)
-    .then(() => (
-      returnSuccess ?
-        Promise.resolve({
-          socialUserId: '0000000000000001',
-          name: 'Dummy Test-Guy',
-          email: 'test@user.com',
-          image: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12998676_1087630357960244_4882584167328260793_n.jpg?oh=3b85a0ae065c63668282cca5f445aea9&oe=5B2F64C7',
-          socialAppTokens: {
-            type: 'Facebook',
-            accessToken: 'myfakeaccesscodeortokenorwoteva',
-            expires: 1527275629,
-          },
-        }) :
-        Promise.reject(new Error('Failed logging in with Google'))
-    ));
-
-export const fakeGoogleLogin = (returnSuccess = true) =>
-  delay(500)
-    .then(() => (
-      returnSuccess ?
-        Promise.resolve({
-          email: 'test.userh@gmail.com',
-          image: 'https://lh3.googleusercontent.com/-c2s_eAAdRP8/AAAAAAAAAAI/AAAAAAAACGE/jreSaPOAvb0/photo.jpg',
-          name: 'Test User',
-          socialAppTokens: {
-            type: 'Google',
-            idToken: 'mygooglyfaceidtokes',
-            refreshToken: 'mygooglyfacerefreshtokes',
-            serverAuthCode: 'mygooglyfaceservertokes',
-            accessToken: 'ohmygoodnessitsmyaccesstokes',
-          },
-          socialUserId: '111111111111111111110',
-        }) :
-        Promise.reject(new Error('Failed logging in with Google'))
-    ));
